c42e2b686778a77b9ad84d2b55272845f8a01d17
rushdanafis <130189960+rushdanafis@users.noreply.github.com>
2023-07-26 00:44:29 +0530

    updated

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ function constructUrl(text){
     fetch(constructUrl(input.value))
     .then(response=>response.json())
     .then(response=>{
+
         output.innerText=response.content.translated
     })
 })
