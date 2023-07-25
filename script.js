aa6abdaf3d7fcd03e959dad95d872d904c097782
rushdanafis <130189960+rushdanafis@users.noreply.github.com>
2023-07-26 00:35:23 +0530

    updated script

---
+++ script.js
@@ -0,0 +1,19 @@
+let input =document.querySelector("#input-element");
+let translateBtn= document.querySelector(".Translate-button");
+let output= document.querySelector(".output");
+let url="https://api.funtranslations.com/translate/minion.json"
+
+
+function constructUrl(text){
+    return url +"?"+"text="+text
+}
+
+ translateBtn.addEventListener("click",function translator(){
+    console.log("clicked");
+    console.log(input.value)
+    fetch(constructUrl(input.value))
+    .then(response=>response.json())
+    .then(response=>{
+        output.innerText=response.content.translated
+    })
+})
